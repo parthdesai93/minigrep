96cbc216c78d9bf5668d4eb6e77e9963ca422bee
Parth Desai <psdesai.93@gmail.com>
2018-03-03 01:07:18 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,28 @@ use std::io::prelude::*;
 fn main() {
   let args: Vec<String> = env::args().collect();
 
-  let query = &args[1];
-  let filename = &args[2];
+  let config = parse_config(&args);
 
-  println!("query: {:?}", query);
-  println!("filename: {:?}", filename);
+  println!("query: {:?}", config.query);
+  println!("filename: {:?}", config.filename);
 
-  let mut f = File::open(filename).expect("File not found");
+  let mut f = File::open(config.filename).expect("File not found");
 
   let mut contents = String::new();
   f.read_to_string(&mut contents)
     .expect("Something went wrong reading file");
 
   println!("With text:\n{}", contents);
+}
+
+struct Config {
+  filename: String,
+  query: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+  let query = args[1].clone();
+  let filename = args[2].clone();
+
+  Config { query, filename }
 }
