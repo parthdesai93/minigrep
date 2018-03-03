c0dd391a4e1d41290de5f7ccb4308c7e3b42f13c
Parth Desai <psdesai.93@gmail.com>
2018-03-03 01:34:00 -0500

    refactor code into lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,25 @@
+extern crate minigrep;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
 
-  let config = Config::new(&args);
+  let config = Config::new(&args).unwrap_or_else(|err| {
+    println!("problem parsing arguments: {}", err);
+    process::exit(1);
+  });
 
   println!("query: {:?}", config.query);
   println!("filename: {:?}", config.filename);
 
-  let mut f = File::open(config.filename).expect("File not found");
-
-  let mut contents = String::new();
-  f.read_to_string(&mut contents)
-    .expect("Something went wrong reading file");
-
-  println!("With text:\n{}", contents);
+  if let Err(e) = minigrep::run(config) {
+    println!("Application Error: {}", e);
+    
+    process::exit(1)
+  }; 
 }
 
-struct Config {
-  filename: String,
-  query: String,
-}
-
-impl Config {
-  fn new(args: &[String]) -> Result<Config, &'static str> {
-    if args.len() < 3 {
-      return Err("not enough arguments");
-    }
-
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
-  }
-}
