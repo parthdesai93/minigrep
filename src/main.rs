e606aaea037cc09f16d9ec70f32c391987cc6a00
Parth Desai <psdesai.93@gmail.com>
2018-03-03 23:07:02 -0500

    print errors using std err stream

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,12 @@ fn main() {
   let args: Vec<String> = env::args().collect();
 
   let config = Config::new(&args).unwrap_or_else(|err| {
-    println!("problem parsing arguments: {}", err);
+    eprintln!("problem parsing arguments: {}", err);
     process::exit(1);
   });
 
-  // println!("query: {:?}", config.query);
-  // println!("filename: {:?}", config.filename);
-
   if let Err(e) = minigrep::run(config) {
-    println!("Application Error: {}", e);
-    
+    eprintln!("Application Error: {}", e);
     process::exit(1)
   }; 
 }
